a72652638da8a0456ce85f495a5d5f8326965c91
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-05-28 21:35:34 +0530

    initial project git

---
+++ src/main/java/in/ogmatech/edutech/edutechservice/EdutechserviceApplication.java
@@ -0,0 +1,12 @@
+package in.ogmatech.edutech.edutechservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechserviceApplication.class, args);
+	}
+}
